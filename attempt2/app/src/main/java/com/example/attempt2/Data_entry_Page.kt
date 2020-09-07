7cb98d858946123e07a61cdd7fbb57734ef39d15
iqbalhusnain <53033529+iqbalhusnain@users.noreply.github.com>
2020-09-07 14:36:05 +0500

    Unresolved reference: view_pager1
    e: C:\Users\user\Documents\GitHub\NFLP\attempt2\app\src\main\java\com\example\attempt2\Data_entry_Page.kt: (19, 54): Unresolved reference: view_pager1

--- attempt2/app/src/main/java/com/example/attempt2/Data_entry_Page.kt
+++ attempt2/app/src/main/java/com/example/attempt2/Data_entry_Page.kt
@@ -18,8 +18,8 @@ class DataEntryPage : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        //val tabs: TabLayout = findViewById(R.id.tabs)
+        //tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
